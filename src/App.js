102e1864f2d159dd6723711e504d1366bca3faf1
Hatice AKGÜL <katrancihatice@icloud.com>
2024-11-15 18:40:55 +0300

    basenama updt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-     <BrowserRouter basename='/'>
+     <BrowserRouter basename='/fintech'>
       <Routes>
         <Route path="/" element={<SignUpSignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
